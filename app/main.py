fdbda728af45b12f96665d3f49b6f12447d35739
holod <vladisjack22@gmail.com>
2026-06-10 15:29:50 +0300

    logging

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, APIRouter
-from fastapi.staticfiles import  StaticFiles
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
+from loguru import logger
+from uuid import uuid4
 
-from app.routers import categories, products,users,reviews,cart,orders,payments
+from app.routers import categories, products, users, reviews, cart, orders, payments
 
 # Создаём приложение FastAPI
 app = FastAPI(
@@ -9,7 +12,28 @@ app = FastAPI(
     version="0.1.0"
 )
 
-app.mount('/media',StaticFiles(directory='media'),name='media')
+
+@app.middleware('http')
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f'Request to {request.url.path} failed')
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f'Request to {request.url.path} failed: {ex}')
+            response = JSONResponse(content={'success': False},status_code=500)
+        return response
+
+
+logger.add("info.log",
+           format="Log: [{extra[log_id]}:{time} - {level} - {message}]",
+           level="INFO", enqueue=True)
+
+app.mount('/media', StaticFiles(directory='media'), name='media')
 # Подключаем маршруты категорий
 
 app.include_router(categories.router)
@@ -20,6 +44,7 @@ app.include_router(cart.router)
 app.include_router(orders.router)
 app.include_router(payments.router)
 
+
 # Корневой эндпоинт для проверки
 @app.get("/")
 async def root():
@@ -27,8 +52,3 @@ async def root():
     Корневой маршрут, подтверждающий, что API работает.
     """
     return {"message": "Добро пожаловать в API интернет-магазина!"}
-
-
-
-
-
